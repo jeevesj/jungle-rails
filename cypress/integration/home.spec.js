306bc754aad038a00fbc2604ece0f24bc289f14f
Christian Gravel <cjgravel@gmail.com>
2023-06-09 12:04:25 -0700

    home cypress test, verify products on page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,9 @@
+
+describe('home', function () {
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+  it("There is 2 products on the page", () => {
+    cy.get(".products article").should("have.length", 2);
+  });
+})
